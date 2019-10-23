17b4331d83df4c82d1dff9360b21b2a630f518ce
deogonit <vlasyukbogdan.ted@gmail.com>
2019-10-23 22:27:33 +0300

    Add markdown for creating posts

--- portal/forum/forms.py
+++ portal/forum/forms.py
@@ -6,11 +6,19 @@ class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
         widget=forms.Textarea(
             attrs={
-                'class': 'form-control',
+                'rows': 5,
                 'placeholder': 'What is on your mind?'
             }),
         max_length=400,
         help_text='The max length of  the text is 400'
+    )
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
     )
 
     class Meta:
@@ -19,26 +27,9 @@ class NewTopicForm(forms.ModelForm):
             'name',
             'message'
         ]
-        widgets = {
-            'name': forms.TextInput(
-                attrs={
-                    'class': 'form-control'
-                }
-            ),
-        }
 
 
 class PostForm(forms.ModelForm):
-    message = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'What is on your mind?'
-            }),
-        max_length=400,
-        help_text='The max length of  the text is 400'
-    )
-
     class Meta:
         model = Post
         fields = [
