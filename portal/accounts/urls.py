80ae2f831bcf70975c5a2e6f936de9d2ec12b1c9
deogonit <vlasyukbogdan.ted@gmail.com>
2019-11-08 23:51:54 +0200

    Add a log in template

--- portal/accounts/urls.py
+++ portal/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth.views import LogoutView, LoginView
+
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -116,11 +116,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+LOGOUT_REDIRECT_URL = 'main_site'
+
+LOGIN_REDIRECT_URL = 'main_site'
